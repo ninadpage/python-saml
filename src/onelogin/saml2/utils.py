0d70e23bc72173a820e563ccd6ddb25469c05e09
ninadpdr <ninad.page@druva.com>
2015-10-29 19:09:29 +0530

    Change AuthnRequest ID prefix

--- src/onelogin/saml2/utils.py
+++ src/onelogin/saml2/utils.py
@@ -391,7 +391,7 @@ class OneLogin_Saml2_Utils(object):
         :return: A unique string
         :rtype: string
         """
-        return 'ONELOGIN_%s' % sha1(uuid4().hex).hexdigest()
+        return 'DR_%s' % sha1(uuid4().hex).hexdigest()
 
     @staticmethod
     def parse_time_to_SAML(time):
